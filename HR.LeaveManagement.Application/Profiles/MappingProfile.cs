874811e1b5b2fdaa33ed00f70b301334c77abefa
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-23 21:14:57 +0300

    Create mapping

---
+++ HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveAllLocation, LeaveAllLocationDto>().ReverseMap();
+        }
+    }
+}
