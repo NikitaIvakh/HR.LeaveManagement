ab7d93b2e87e957096f383f07140b80335162fb0
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-23 20:18:09 +0300

    create a generalized repository interface

---
+++ HR.LeaveManagement.Application/Persistence/Contracts/IGeneticRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace HR.LeaveManagement.Application.Persistence.Contracts
+{
+    public interface IGeneticRepository<T> where T : class
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+
+        Task<T> GetAsync(int id);
+
+        Task<T> CreateAsync(T entity);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task<T> DeleteAsync(T entity);
+    }
+}
