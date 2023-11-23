16d705826c15762dbf3a1d00f858d2c79c511a38
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-23 20:52:00 +0300

    Create Dto classes

---
+++ HR.LeaveManagement.Application/DTOs/LeaveAllLocationDto.cs
@@ -0,0 +1,15 @@
+﻿using HR.LeaveManagement.Application.DTOs.Common;
+
+namespace HR.LeaveManagement.Application.DTOs
+{
+    public class LeaveAllLocationDto : BaseDto
+    {
+        public int NumbersOfDays { get; set; }
+
+        public LeaveTypeDto LeaveType { get; set; }
+
+        public int LeaveTypeId { get; set; }
+
+        public int Period { get; set; }
+    }
+}
