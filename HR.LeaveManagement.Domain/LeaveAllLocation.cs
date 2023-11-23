4e17c6b83c8defec1a8b6723d806434d6c724098
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-23 19:15:15 +0300

    Create a LeaveAllLocation class

---
+++ HR.LeaveManagement.Domain/LeaveAllLocation.cs
@@ -0,0 +1,17 @@
+﻿namespace HR.LeaveManagement.Domain
+{
+    public class LeaveAllLocation
+    {
+        public int Id { get; set; }
+
+        public int NumbersOfDays { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public LeaveType LeaveType { get; set; }
+
+        public int LeaveTypeId { get; set; }
+
+        public int Period { get; set; }
+    }
+}
