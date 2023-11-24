8f160b0702cb4fb3ef65d86ae5abe2ac2f4fd4c9
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-24 19:29:07 +0300

    Create the UpdateLeaveRequestDto and ChangeLeaveRequestApplovalDto class

---
+++ HR.LeaveManagement.Application/DTOs/LeaveRequest/ChangeLeaveRequestApplovalDto.cs
@@ -0,0 +1,9 @@
+﻿using HR.LeaveManagement.Application.DTOs.Common;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveRequest
+{
+    public class ChangeLeaveRequestApplovalDto : BaseDto
+    {
+        public bool? Approved { get; set; }
+    }
+}
