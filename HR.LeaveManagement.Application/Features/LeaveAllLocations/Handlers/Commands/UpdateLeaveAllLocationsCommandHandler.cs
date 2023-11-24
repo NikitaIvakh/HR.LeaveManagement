f4e1301d617ac8a4b98834f6ff0a556e1fdc86bf
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-24 20:02:12 +0300

    Create a command to update the location

---
+++ HR.LeaveManagement.Application/Features/LeaveAllLocations/Handlers/Commands/UpdateLeaveAllLocationsCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveAllLocations.Requests.Commands;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllLocations.Handlers.Commands
+{
+    public class UpdateLeaveAllLocationsCommandHandler : IRequestHandler<UpdateLeaveAllLocationsCommand, Unit>
+    {
+        private readonly ILeaveAllLocationRepository _leaveAllLocationRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateLeaveAllLocationsCommandHandler(ILeaveAllLocationRepository leaveAllLocationRepository, IMapper mapper)
+        {
+            _leaveAllLocationRepository = leaveAllLocationRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdateLeaveAllLocationsCommand request, CancellationToken cancellationToken)
+        {
+            var leaveAllLocation = await _leaveAllLocationRepository.GetAsync(request.LeaveAllLocationDto.Id);
+            _mapper.Map(request.LeaveAllLocationDto, leaveAllLocation);
+            await _leaveAllLocationRepository.UpdateAsync(leaveAllLocation);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveAllLocations/Requests/Commands/UpdateLeaveAllLocationsCommand.cs
@@ -0,0 +1,10 @@
+﻿using HR.LeaveManagement.Application.DTOs.LeaveAllLocation;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllLocations.Requests.Commands
+{
+    public class UpdateLeaveAllLocationsCommand : IRequest<Unit>
+    {
+        public UpdateLeaveAllLocationDto LeaveAllLocationDto { get; set; }
+    }
+}
