ce9afcafcab1cc310793afbc995baa8a1891dd82
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-24 10:57:25 +0300

    Process a request for a specific trip

---
+++ HR.LeaveManagement.Application/Features/LeaveTypes/Handlers/Queries/GetLeaveTypeDetailsRequestHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using AutoMapper;
+using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Queries;
+
+namespace HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Queries
+{
+    public class GetLeaveTypeDetailsRequestHandler : IRequestHandler<GetLeaveTypeDetailsRequest, LeaveTypeDto>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeaveTypeDetailsRequestHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<LeaveTypeDto> Handle(GetLeaveTypeDetailsRequest request, CancellationToken cancellationToken)
+        {
+            var leaveType = await _leaveTypeRepository.GetAsync(request.Id);
+            return _mapper.Map<LeaveTypeDto>(leaveType);
+        }
+    }
+}
