58749662ba653d37b1d0aef8fd9dc750f43b8c1d
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-24 10:41:20 +0300

    Process a request to get a list

---
+++ HR.LeaveManagement.Application/Features/LeaveTypes/Handlers/Queries/GetLeaveTypeListRequestHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Queries;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using AutoMapper;
+
+namespace HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Queries
+{
+    public class GetLeaveTypeListRequestHandler : IRequestHandler<GetLeaveTypeListRequest, List<LeaveTypeDto>>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeaveTypeListRequestHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypeListRequest request, CancellationToken cancellationToken)
+        {
+            var leaveTypes = await _leaveTypeRepository.GetAllAsync();
+            return _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
+        }
+    }
+}
