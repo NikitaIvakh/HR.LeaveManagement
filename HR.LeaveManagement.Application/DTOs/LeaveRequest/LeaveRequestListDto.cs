05029c9c981e76574e63ff11816ad283378038dc
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-24 09:52:07 +0300

    Add the LeaveRequest folder , and create a class in it to display the request and the list of requests

---
+++ HR.LeaveManagement.Application/DTOs/LeaveRequest/LeaveRequestListDto.cs
@@ -0,0 +1,13 @@
+﻿using HR.LeaveManagement.Application.DTOs.Common;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveRequest
+{
+    public class LeaveRequestListDto : BaseDto
+    {
+        public LeaveTypeDto LeaveType { get; set; }
+
+        public DateTime DateRequest { get; set; }
+
+        public bool? Approved { get; set; }
+    }
+}
--- HR.LeaveManagement.Application/Profiles/MappingProfile.cs
+++ HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Application.DTOs.LeaveRequest;
 using HR.LeaveManagement.Domain;
 
 namespace HR.LeaveManagement.Application.Profiles
