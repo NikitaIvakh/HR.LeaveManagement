221d4ca37e7f3a34c49ccfaec8f35bf677bfe315
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-24 19:22:24 +0300

    Create the UpdateLeaveAllLocationDto class

---
+++ HR.LeaveManagement.Application/DTOs/LeaveAllLocation/UpdateLeaveAllLocationDto.cs
@@ -0,0 +1,13 @@
+﻿using HR.LeaveManagement.Application.DTOs.Common;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveAllLocation
+{
+    public class UpdateLeaveAllLocationDto : BaseDto
+    {
+        public int NumbersOfDays { get; set; }
+
+        public int LeaveTypeId { get; set; }
+
+        public int Period { get; set; }
+    }
+}
