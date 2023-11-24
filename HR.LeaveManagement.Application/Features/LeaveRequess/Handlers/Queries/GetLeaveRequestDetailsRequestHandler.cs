bf846bd91eb9ce41b963c8ef266cf29bf96416fe
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-24 11:14:04 +0300

    Add logic for receiving a request by Id

---
+++ HR.LeaveManagement.Application/Features/LeaveRequess/Handlers/Queries/GetLeaveRequestDetailsRequestHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using HR.LeaveManagement.Application.DTOs.LeaveRequest;
+using HR.LeaveManagement.Application.Features.LeaveRequess.Requests.Queries;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using AutoMapper;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequess.Handlers.Queries
+{
+    public class GetLeaveRequestDetailsRequestHandler : IRequestHandler<GetLeaveRequestDetailsRequest, LeaveRequestDto>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeaveRequestDetailsRequestHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<LeaveRequestDto> Handle(GetLeaveRequestDetailsRequest request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = await _leaveRequestRepository.GetAsync(request.Id);
+            return _mapper.Map<LeaveRequestDto>(leaveRequest);
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveRequess/Requests/Queries/GetLeaveRequestDetailsRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using HR.LeaveManagement.Application.DTOs.LeaveRequest;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequess.Requests.Queries
+{
+    public class GetLeaveRequestDetailsRequest : IRequest<LeaveRequestDto>
+    {
+        public int Id { get; set; }
+    }
+}
