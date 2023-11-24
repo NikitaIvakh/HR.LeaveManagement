21a2e8da9dfc581cddaf9a64e1d3578d20023285
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-24 18:28:20 +0300

    Add a command to create a vacation request

---
+++ HR.LeaveManagement.Application/Features/LeaveRequess/Handlers/Commands/CreateLeaveRequessCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveRequess.Requests.Commands;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using HR.LeaveManagement.Domain;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequess.Handlers.Commands
+{
+    public class CreateLeaveRequessCommandHandler : IRequestHandler<CreateLeaveRequessCommand, int>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IMapper _mapper;
+
+        public CreateLeaveRequessCommandHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<int> Handle(CreateLeaveRequessCommand request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = _mapper.Map<LeaveRequest>(request.LeaveRequestDto);
+            leaveRequest = await _leaveRequestRepository.CreateAsync(leaveRequest);
+
+            return leaveRequest.Id;
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveRequess/Requests/Commands/CreateLeaveRequessCommand.cs
@@ -0,0 +1,10 @@
+﻿using HR.LeaveManagement.Application.DTOs.LeaveRequest;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequess.Requests.Commands
+{
+    public class CreateLeaveRequessCommand : IRequest<int>
+    {
+        public LeaveRequestDto LeaveRequestDto { get; set; }
+    }
+}
