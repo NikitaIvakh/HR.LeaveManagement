f9826d5b4a2d3e7441b887cc3bd28eccc7766fe9
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-25 10:48:03 +0300

    Add a command to delete a vacation request

---
+++ HR.LeaveManagement.Application/Features/LeaveRequess/Handlers/Commands/DeleteLeaveRequessCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequess.Handlers.Commands
+{
+    public class DeleteLeaveRequessCommandHandler : IRequestHandler<DeleteLeaveTypeCommand, Unit>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+
+        public DeleteLeaveRequessCommandHandler(ILeaveRequestRepository leaveRequestRepository)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = await _leaveRequestRepository.GetAsync(request.Id);
+            await _leaveRequestRepository.DeleteAsync(leaveRequest);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveRequess/Requests/Commands/DeleteLeaveRequessCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequess.Requests.Commands
+{
+    public class DeleteLeaveRequessCommand : IRequest<Unit>
+    {
+        public int Id { get; set; }
+    }
+}
