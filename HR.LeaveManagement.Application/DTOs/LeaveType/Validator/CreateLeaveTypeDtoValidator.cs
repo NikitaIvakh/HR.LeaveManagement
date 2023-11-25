9b995ddd9daea541059bd815f06b02956516abb7
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-25 15:31:49 +0300

    Add validation to create a vacation type

---
+++ HR.LeaveManagement.Application/DTOs/LeaveType/Validator/CreateLeaveTypeDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveType.Validator
+{
+    public class CreateLeaveTypeDtoValidator : AbstractValidator<CreateLeaveTypeDto>
+    {
+        public CreateLeaveTypeDtoValidator()
+        {
+            RuleFor(key => key.Name)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull().MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
+
+            RuleFor(key => key.DefaultDays)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .GreaterThan(0).WithMessage("{PropertyName} must be at least 1.")
+                .LessThan(100).WithMessage("{PropertyName} must be less than 100.");
+        }
+    }
+}
