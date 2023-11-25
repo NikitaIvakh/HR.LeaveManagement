fbde6a0e8251f5ba68e69805cf58e6008bc342d4
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-25 10:44:04 +0300

    Add a command to delete a vacation type

---
+++ HR.LeaveManagement.Application/Features/LeaveTypes/Handlers/Commands/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Commands
+{
+    public class DeleteLeaveTypeCommandHandler : IRequestHandler<DeleteLeaveTypeCommand, Unit>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public DeleteLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            var leaveType = await _leaveTypeRepository.GetAsync(request.Id);
+            await _leaveTypeRepository.DeleteAsync(leaveType);
+
+            return Unit.Value;
+        }
+    }
+}
