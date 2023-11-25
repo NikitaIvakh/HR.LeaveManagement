0ee93f4a7845cf7e8d3e8f213a583dbf41ef5e28
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-25 17:03:06 +0300

    Apply validation to location creation

---
+++ HR.LeaveManagement.Application/DTOs/LeaveAllLocation/Validators/CreateLeaveAllLocationDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveAllLocation.Validators
+{
+    public class CreateLeaveAllLocationDtoValidator : AbstractValidator<CreateLeaveAllLocationDto>
+    {
+        public CreateLeaveAllLocationDtoValidator()
+        {
+            RuleFor(key => key.NumbersOfDays)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .GreaterThan(0).WithMessage("{PropertyName} must be at least 1.")
+                .LessThan(60).WithMessage("{PropertyName} must be less than {ComprasionValue}.");
+
+            RuleFor(key => key.LeaveTypeId)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull();
+
+            RuleFor(key => key.Period)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .GreaterThan(0).WithMessage("{PropertyName} must be at least 1.")
+                .LessThan(365).WithMessage("{PropertyName} must be less than {ComprasionValue}.");
+        }
+    }
+}
--- HR.LeaveManagement.Application/Features/LeaveAllLocations/Handlers/Commands/CreateLeaveAllLocationsCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveAllLocations/Handlers/Commands/CreateLeaveAllLocationsCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTOs.LeaveAllLocation.Validators;
 using HR.LeaveManagement.Application.Features.LeaveAllLocations.Requests.Commands;
 using HR.LeaveManagement.Application.Persistence.Contracts;
 using HR.LeaveManagement.Domain;
@@ -19,6 +20,12 @@ namespace HR.LeaveManagement.Application.Features.LeaveAllLocations.Handlers.Com
 
         public async Task<int> Handle(CreateLeaveAllLocationsCommand request, CancellationToken cancellationToken)
         {
+            var validator = new CreateLeaveAllLocationDtoValidator();
+            var validationResult = await validator.ValidateAsync(request.LeaveAllLocationDto, cancellationToken);
+
+            if (validationResult.IsValid is false)
+                throw new Exception();
+
             var leaveAllLocation = _mapper.Map<LeaveAllLocation>(request.LeaveAllLocationDto);
             leaveAllLocation = await _leaveAllLocationRepository.CreateAsync(leaveAllLocation);
 
