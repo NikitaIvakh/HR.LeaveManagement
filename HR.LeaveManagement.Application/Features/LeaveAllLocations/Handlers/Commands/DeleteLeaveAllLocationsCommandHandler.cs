f73156e2d204b91c5b884f725da8dca43ee709e1
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-25 10:56:12 +0300

    Add a command to delete vacation information

---
+++ HR.LeaveManagement.Application/Features/LeaveAllLocations/Handlers/Commands/DeleteLeaveAllLocationsCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using HR.LeaveManagement.Application.Features.LeaveAllLocations.Requests.Commands;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllLocations.Handlers.Commands
+{
+    public class DeleteLeaveAllLocationsCommandHandler : IRequestHandler<DeleteLeaveAllLocationsCommand, Unit>
+    {
+        private readonly ILeaveAllLocationRepository _leaveAllLocationRepository;
+
+        public DeleteLeaveAllLocationsCommandHandler(ILeaveAllLocationRepository leaveAllLocationRepository)
+        {
+            _leaveAllLocationRepository = leaveAllLocationRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveAllLocationsCommand request, CancellationToken cancellationToken)
+        {
+            var leaveAllLocation = await _leaveAllLocationRepository.GetAsync(request.Id);
+            await _leaveAllLocationRepository.DeleteAsync(leaveAllLocation);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveAllLocations/Requests/Commands/DeleteLeaveAllLocationsCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllLocations.Requests.Commands
+{
+    public class DeleteLeaveAllLocationsCommand : IRequest<Unit>
+    {
+        public int Id { get; set; }
+    }
+}
