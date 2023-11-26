e735e3c6363e26d7faa8cfb2d0f97a9e8eb269d3
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-26 14:47:39 +0300

    Implement a repository with the LeaveType class

---
+++ HR.LeaveManagement.Persistence/Repositories/LeaveTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using HR.LeaveManagement.Application.Persistence.Contracts;
+using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Persistence.Repositories
+{
+    public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
+    {
+        private readonly HRLeaveManagementDbContext _context;
+
+        public LeaveTypeRepository(HRLeaveManagementDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
