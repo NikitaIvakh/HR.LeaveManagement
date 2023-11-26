dd73ce8361a1d00ccd412d1faac56aa6a75c73aa
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-26 15:03:21 +0300

    Implement a repository with the LeaveAllLocation class

---
+++ HR.LeaveManagement.Persistence/Repositories/LeaveAllLocationRepository.cs
@@ -0,0 +1,28 @@
+﻿using HR.LeaveManagement.Application.Persistence.Contracts;
+using HR.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR.LeaveManagement.Persistence.Repositories
+{
+    public class LeaveAllLocationRepository : GenericRepository<LeaveAllLocation>, ILeaveAllLocationRepository
+    {
+        private readonly HRLeaveManagementDbContext _context;
+
+        public LeaveAllLocationRepository(HRLeaveManagementDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<LeaveAllLocation>> GetAllLocationsWithDetails()
+        {
+            var leaveAllLocations = await _context.LeaveAllLocations.Include(key => key.LeaveType).ToListAsync();
+            return leaveAllLocations;
+        }
+
+        public async Task<LeaveAllLocation> GetLeaveAllLocationWithDetails(int id)
+        {
+            var leaveAllLocation = await _context.LeaveAllLocations.Include(key => key.LeaveType).FirstAsync(key => key.Id == id);
+            return leaveAllLocation;
+        }
+    }
+}
