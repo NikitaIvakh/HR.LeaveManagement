76d6d317576bd1e6ed9b0b2de3b4822e08c509a2
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-26 10:19:15 +0300

    Create an exception for validation

---
+++ HR.LeaveManagement.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HR.LeaveManagement.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public ValidationException(ValidationResult validationResult)
+        {
+            Errors = new List<string>();
+
+            foreach (var error in validationResult.ErrorMessage)
+            {
+                Errors.Add(error.ToString());
+            }
+        }
+
+        public List<string> Errors { get; set; }
+    }
+}
