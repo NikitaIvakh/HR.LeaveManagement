fadee190cdf49279ae118a1e318c3262239b9006
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-26 11:12:35 +0300

    Create a BaseCommandResponse class

---
+++ HR.LeaveManagement.Application/Responses/BaseCommandResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace HR.LeaveManagement.Application.Responses
+{
+    public class BaseCommandResponse
+    {
+        public bool Success { get; set; } = true;
+
+        public List<string> Errors { get; set; }
+
+        public string Message { get; set; }
+    }
+}
