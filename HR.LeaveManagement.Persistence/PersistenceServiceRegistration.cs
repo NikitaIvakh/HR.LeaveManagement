4fa2d145a366e1c3d5ea6195a927482889d63002
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-26 15:14:45 +0300

    Create a class for registering services

---
+++ HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using HR.LeaveManagement.Application.Persistence.Contracts;
+using HR.LeaveManagement.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<HRLeaveManagementDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("HRLeaveManagementConnectionString"));
+            });
+
+            services.AddScoped(typeof(IGeneticRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+            services.AddScoped<ILeaveAllLocationRepository, LeaveAllLocationRepository>();
+
+            return services;
+        }
+    }
+}
