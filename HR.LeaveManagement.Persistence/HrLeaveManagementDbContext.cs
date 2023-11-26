11ab457b6ceb02e56c39d993efdca078164abe33
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-26 13:28:46 +0300

    Create a Database Context

---
+++ HR.LeaveManagement.Persistence/HrLeaveManagementDbContext.cs
@@ -0,0 +1,38 @@
+﻿using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public class HRLeaveManagementDbContext : DbContext
+    {
+        public HRLeaveManagementDbContext(DbContextOptions<HRLeaveManagementDbContext> options) : base(options) { }
+
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+
+        public DbSet<LeaveAllLocation> LeaveAllLocations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(HRLeaveManagementDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;
+
+                if (entry.State is EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
