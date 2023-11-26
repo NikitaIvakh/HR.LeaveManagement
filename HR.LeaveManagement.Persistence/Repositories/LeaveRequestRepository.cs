8de3c5cf2b6644895d1a7873ef131ff0b89311bc
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-26 14:58:10 +0300

    Implement a repository with the LeaveRequest class

---
+++ HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -0,0 +1,35 @@
+﻿using HR.LeaveManagement.Application.Persistence.Contracts;
+using HR.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR.LeaveManagement.Persistence.Repositories
+{
+    public class LeaveRequestRepository : GenericRepository<LeaveRequest>, ILeaveRequestRepository
+    {
+        private readonly HRLeaveManagementDbContext _context;
+
+        public LeaveRequestRepository(HRLeaveManagementDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails()
+        {
+            var leaveRequests = await _context.LeaveRequests.Include(key => key.LeaveType).ToListAsync();
+            return leaveRequests;
+        }
+
+        public async Task<LeaveRequest> GetLeaveRequestWithDetails(int id)
+        {
+            var leaveRequest = await _context.LeaveRequests.Include(key => key.LeaveType).FirstAsync(key => key.Id == id);
+            return leaveRequest;
+        }
+
+        public async Task ChangeApprovalStatus(LeaveRequest leaveRequest, bool? ApprovalStatus)
+        {
+            leaveRequest.Approved = ApprovalStatus;
+            _context.Entry(leaveRequest).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
