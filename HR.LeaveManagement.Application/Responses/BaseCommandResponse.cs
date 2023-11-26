b8ab9c0f657e4984690c61e101e8c961b30e5305
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-26 11:14:54 +0300

    Update the BaseCommandResponse class

--- HR.LeaveManagement.Application/Responses/BaseCommandResponse.cs
+++ HR.LeaveManagement.Application/Responses/BaseCommandResponse.cs
@@ -2,6 +2,8 @@
 {
     public class BaseCommandResponse
     {
+        public int Id { get; set; }
+
         public bool Success { get; set; } = true;
 
         public List<string> Errors { get; set; }
