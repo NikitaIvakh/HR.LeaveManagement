75a4729dbef37dbca8fa9c20ef1e7c7e24e31cbf
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-27 09:40:56 +0300

    Apply working with mail in a team to create a vacation

--- HR.LeaveManagement.Application/Features/LeaveRequess/Handlers/Commands/CreateLeaveRequessCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveRequess/Handlers/Commands/CreateLeaveRequessCommandHandler.cs
@@ -5,6 +5,9 @@ using HR.LeaveManagement.Application.Contracts.Persistence;
 using HR.LeaveManagement.Application.Responses;
 using HR.LeaveManagement.Domain;
 using MediatR;
+using HR.LeaveManagement.Application.Contracts.Infrastructure;
+using HR.LeaveManagement.Application.Models;
+using HR.LeaveManagement.Application.Exceptions;
 
 namespace HR.LeaveManagement.Application.Features.LeaveRequess.Handlers.Commands
 {
@@ -12,11 +15,13 @@ namespace HR.LeaveManagement.Application.Features.LeaveRequess.Handlers.Commands
     {
         private readonly ILeaveRequestRepository _leaveRequestRepository;
         private readonly IMapper _mapper;
+        private readonly IEmailSender _emailSender;
 
-        public CreateLeaveRequessCommandHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        public CreateLeaveRequessCommandHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper, IEmailSender emailSender)
         {
             _leaveRequestRepository = leaveRequestRepository;
             _mapper = mapper;
+            _emailSender = emailSender;
         }
 
         public async Task<BaseCommandResponse> Handle(CreateLeaveRequessCommand request, CancellationToken cancellationToken)
@@ -38,6 +43,24 @@ namespace HR.LeaveManagement.Application.Features.LeaveRequess.Handlers.Commands
             response.Success = true;
             response.Message = "Creation Successful";
             response.Id = leaveRequest.Id;
+
+            var email = new Email
+            {
+                To = "nikita.ivakh7@gmail.com",
+                Body = $"Your leave request for {request.LeaveRequestDto.StartDate:D} to {request.LeaveRequestDto.EndDate:D} " +
+                $"has been submited successfully.",
+                Subject = "Leave request submited",
+            };
+
+            try
+            {
+                await _emailSender.SendEmailAsync(email);
+            }
+
+            catch
+            {
+                throw new BadRequestException("Internal Server Error");
+            }
 
             return response;
         }
