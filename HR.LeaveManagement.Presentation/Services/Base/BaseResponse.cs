941da64eb9807f2a99fb90891d0638873da584b2
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-29 20:45:15 +0300

    Add a BaseResponse class

---
+++ HR.LeaveManagement.Presentation/Services/Base/BaseResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace HR.LeaveManagement.Presentation.Services.Base
+{
+    public class BaseResponse<Type>
+    {
+        public string Message { get; set; }
+
+        public string ValidationErrors { get; set; }
+
+        public bool Status { get; set; }
+
+        public Type Data { get; set; }
+    }
+}
