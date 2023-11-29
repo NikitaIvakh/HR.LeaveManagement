4326513a0c62d3536061312be13b1a651252e85e
Nikita Ivakh <maffy145rus@mail.ru>
2023-11-29 18:42:44 +0300

    Create a configuration file for the LeaveAllLocation class

---
+++ HR.LeaveManagement.Persistence/Configurations/Entities/LeaveAllLocationConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using HR.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HR.LeaveManagement.Persistence.Configurations.Entities
+{
+    public class LeaveAllLocationConfiguration : IEntityTypeConfiguration<LeaveAllLocation>
+    {
+        public void Configure(EntityTypeBuilder<LeaveAllLocation> builder)
+        {
+
+        }
+    }
+}
