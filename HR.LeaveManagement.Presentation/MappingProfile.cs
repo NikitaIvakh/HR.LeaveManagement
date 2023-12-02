89335f8f66b805de8bf3438dfadb0f11e4f4665d
Nikita Ivakh <maffy145rus@mail.ru>
2023-12-02 17:55:26 +0300

    Add Mapping

---
+++ HR.LeaveManagement.Presentation/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Presentation.Models;
+using HR.LeaveManagement.Presentation.Services.Base;
+
+namespace HR.LeaveManagement.Presentation
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CreateLeaveTypeDto, CreateLeaveTypeViewModel>().ReverseMap();
+            CreateMap<LeaveTypeDto, LeaveTypeViewModel>().ReverseMap();
+        }
+    }
+}
