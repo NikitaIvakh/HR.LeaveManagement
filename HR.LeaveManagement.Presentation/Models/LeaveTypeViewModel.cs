6da1dc9d6f60e7d2ded09d4499d8e7cdcc3e47ef
Nikita Ivakh <maffy145rus@mail.ru>
2023-12-02 17:52:21 +0300

    Add the LeaveTypeViewModel class

---
+++ HR.LeaveManagement.Presentation/Models/LeaveTypeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HR.LeaveManagement.Presentation.Models
+{
+    public class LeaveTypeViewModel : CreateLeaveTypeViewModel
+    {
+        public int Id { get; set; }
+    }
+
+    public class CreateLeaveTypeViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Default number of days")]
+        public int DefaulyDays { get; set; }
+    }
+}
