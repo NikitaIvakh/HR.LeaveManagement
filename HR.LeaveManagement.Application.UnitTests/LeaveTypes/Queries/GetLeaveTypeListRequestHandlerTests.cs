aac71f4cf4dd11eeba062943830f2e2c6e15fa47
Nikita Ivakh <maffy145rus@mail.ru>
2023-12-02 17:11:35 +0300

    Add a test for successful list output

---
+++ HR.LeaveManagement.Application.UnitTests/LeaveTypes/Queries/GetLeaveTypeListRequestHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.DTOs.LeaveType;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Queries;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Queries;
+using HR.LeaveManagement.Application.Profiles;
+using HR.LeaveManagement.Application.UnitTests.Common;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace HR.LeaveManagement.Application.UnitTests.LeaveTypes.Queries
+{
+    public class GetLeaveTypeListRequestHandlerTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ILeaveTypeRepository> _repository;
+
+        public GetLeaveTypeListRequestHandlerTests()
+        {
+            _repository = MockLeaveTypeRepository.GetLeaveTypeRepository();
+
+            var mapperConfig = new MapperConfiguration(key =>
+            {
+                key.AddProfile<MappingProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetLeaveTypeListQuery_Success()
+        {
+            var handler = new GetLeaveTypeListRequestHandler(_repository.Object, _mapper);
+            var result = await handler.Handle(new GetLeaveTypeListRequest(), CancellationToken.None);
+
+            result.ShouldBeOfType<List<LeaveTypeDto>>();
+            result.Count.ShouldBe(3);
+        }
+    }
+}
