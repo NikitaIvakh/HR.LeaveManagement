c21b5808ffce2d9c800476c255362edf98828a68
Nikita Ivakh <maffy145rus@mail.ru>
2023-12-04 17:33:11 +0300

    Create a Database Context

---
+++ HR.LeaveManagement.Application/Contracts/Identity/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using HR.LeaveManagement.Application.Models.Identity;
+
+namespace HR.LeaveManagement.Application.Contracts.Identity
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> LoginAsync(AuthRequest request);
+
+        Task<RegistrationResponse> RegisterAsync(RegistrationRequest request);
+    }
+}
