2316c072ec5c0673cb5e8a5b2b3d3dbf2908ccc5
Nikita Ivakh <maffy145rus@mail.ru>
2023-12-06 17:49:29 +0300

    Add additional logic for Swagger

--- HR.LeaveManagement.API/Program.cs
+++ HR.LeaveManagement.API/Program.cs
@@ -8,6 +8,8 @@ WebApplicationBuilder applicationBuilder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+applicationBuilder.Services.AddHttpContextAccessor();
+AddSwaggerDock(applicationBuilder.Services);
 applicationBuilder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 applicationBuilder.Services.AddEndpointsApiExplorer();
@@ -50,3 +52,46 @@ webApplication.UseEndpoints(endpoints =>
 });
 
 webApplication.Run();
+
+static void AddSwaggerDock(IServiceCollection services)
+{
+    services.AddSwaggerGen(key =>
+    {
+        key.AddSecurityDefinition(name: "Bearer", new OpenApiSecurityScheme
+        {
+            Description = @"JWT Authorization header using the Bearer scheme. 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Scheme",
+        });
+
+        key.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header,
+                },
+
+                new List<string>()
+            }
+        });
+
+        key.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "HR Leave Management API",
+        });
+    });
+}
