5f16acf7bc8df14991579a041535be8722e18f9f
Nikita Ivakh <maffy145rus@mail.ru>
2023-12-07 19:12:02 +0300

    Implement a dependency

--- HR.LeaveManagement.Presentation/Program.cs
+++ HR.LeaveManagement.Presentation/Program.cs
@@ -1,16 +1,24 @@
 using HR.LeaveManagement.Presentation.Contracts;
 using HR.LeaveManagement.Presentation.Services;
 using HR.LeaveManagement.Presentation.Services.Base;
-using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using System.Reflection;
 
 WebApplicationBuilder applicationBuilder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+applicationBuilder.Services.AddHttpContextAccessor();
 applicationBuilder.Services.AddHttpClient<IClient, Client>(key => key.BaseAddress = new Uri("https://localhost:7271"));
 //applicationBuilder.Services.AddSingleton<IClient, Client>();
 applicationBuilder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 applicationBuilder.Services.AddScoped<ILeaveTypeService, LeaveTypeService>();
+
+applicationBuilder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+applicationBuilder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
+applicationBuilder.Services.AddTransient<IAuthenticationService, AuthenticationService>();
 
 applicationBuilder.Services.AddSingleton<ILocalStorageService, LocalStorageService>();
 applicationBuilder.Services.AddControllersWithViews();
@@ -24,6 +32,9 @@ if (!webApplication.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     webApplication.UseHsts();
 }
+
+webApplication.UseCookiePolicy();
+webApplication.UseAuthentication();
 
 webApplication.UseHttpsRedirection();
 webApplication.UseStaticFiles();
