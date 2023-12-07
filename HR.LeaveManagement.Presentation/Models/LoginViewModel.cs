ff70e4c773cd47943be46fd19e871e1de8886d69
Nikita Ivakh <maffy145rus@mail.ru>
2023-12-07 18:25:06 +0300

    Create a LoginViewModel class

---
+++ HR.LeaveManagement.Presentation/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HR.LeaveManagement.Presentation.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
