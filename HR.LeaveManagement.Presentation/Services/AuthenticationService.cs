27af246b13a79374a64c99187dfaae10e30f27c8
Nikita Ivakh <maffy145rus@mail.ru>
2023-12-07 18:18:45 +0300

    Create the IAuthenticationService interface and implement it

---
+++ HR.LeaveManagement.Presentation/Services/AuthenticationService.cs
@@ -0,0 +1,75 @@
+﻿using HR.LeaveManagement.Presentation.Contracts;
+using HR.LeaveManagement.Presentation.Services.Base;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace HR.LeaveManagement.Presentation.Services
+{
+    public class AuthenticationService : BaseHttpService, Contracts.IAuthenticationService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly JwtSecurityTokenHandler _jwtSecurityTokenHandler;
+
+        public AuthenticationService(ILocalStorageService localStorageService, IClient client, IHttpContextAccessor httpContextAccessor) : base(localStorageService, client)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+        }
+
+        public async Task<bool> Authenticate(string email, string password)
+        {
+            try
+            {
+                AuthRequest authRequest = new() { Email = email, Password = password };
+                AuthResponse authenticateResposne = await _client.LoginAsync(authRequest);
+
+                if (authenticateResposne.Token != string.Empty)
+                {
+                    var tokenContent = _jwtSecurityTokenHandler.ReadJwtToken(authenticateResposne.Token);
+                    var claims = ParseClaims(tokenContent);
+                    var user = new ClaimsPrincipal(new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme));
+                    var login = _httpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, user);
+                    _localStorage.SetStorageValue("token", authenticateResposne.Token);
+
+                    return true;
+                }
+
+                return false;
+            }
+
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> Register(string firstName, string lastName, string userName, string email, string password)
+        {
+            RegistrationRequest registrationRequest = new() { FirstName = firstName, LastName = lastName, UserName = userName, EmailAddress = email, Password = password };
+            RegistrationResponse response = await _client.RegisterAsync(registrationRequest);
+
+            if (!string.IsNullOrEmpty(response.UserId))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task Logout()
+        {
+            _localStorage.ClearStorage(new List<string> { "token" });
+            await _httpContextAccessor.HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+        private static IEnumerable<Claim> ParseClaims(JwtSecurityToken tokenContent)
+        {
+            var claims = tokenContent.Claims.ToList();
+            claims.Add(new Claim(ClaimTypes.Name, tokenContent.Subject));
+
+            return claims;
+        }
+    }
+}
