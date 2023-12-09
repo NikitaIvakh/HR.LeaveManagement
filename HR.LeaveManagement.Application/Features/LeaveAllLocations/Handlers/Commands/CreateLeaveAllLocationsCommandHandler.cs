7c3012711913c1ca33c0bc6ef0cbe6fb8dffa808
Nikita Ivakh <maffy145rus@mail.ru>
2023-12-09 10:34:47 +0300

    Add new logic

--- HR.LeaveManagement.Application/Features/LeaveAllLocations/Handlers/Commands/CreateLeaveAllLocationsCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveAllLocations/Handlers/Commands/CreateLeaveAllLocationsCommandHandler.cs
@@ -5,18 +5,23 @@ using HR.LeaveManagement.Application.Contracts.Persistence;
 using HR.LeaveManagement.Application.Responses;
 using HR.LeaveManagement.Domain;
 using MediatR;
+using HR.LeaveManagement.Application.Contracts.Identity;
 
 namespace HR.LeaveManagement.Application.Features.LeaveAllLocations.Handlers.Commands
 {
     public class CreateLeaveAllLocationsCommandHandler : IRequestHandler<CreateLeaveAllLocationsCommand, BaseCommandResponse>
     {
         private readonly ILeaveAllLocationRepository _leaveAllLocationRepository;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IUserService _userService;
         private readonly IMapper _mapper;
 
-        public CreateLeaveAllLocationsCommandHandler(ILeaveAllLocationRepository leaveAllLocationRepository, IMapper mapper)
+        public CreateLeaveAllLocationsCommandHandler(ILeaveAllLocationRepository leaveAllLocationRepository, IMapper mapper, IUserService userService, ILeaveTypeRepository leaveTypeRepository)
         {
             _leaveAllLocationRepository = leaveAllLocationRepository;
             _mapper = mapper;
+            _userService = userService;
+            _leaveTypeRepository = leaveTypeRepository;
         }
 
         public async Task<BaseCommandResponse> Handle(CreateLeaveAllLocationsCommand request, CancellationToken cancellationToken)
@@ -30,6 +35,30 @@ namespace HR.LeaveManagement.Application.Features.LeaveAllLocations.Handlers.Com
                 response.Success = false;
                 response.Message = "Creation Failed";
                 response.Errors = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+            }
+
+            else
+            {
+                var leaveType = await _leaveTypeRepository.GetAsync(request.LeaveAllLocationDto.LeaveTypeId);
+                var employees = await _userService.GetEmployeesAsync();
+                var period = DateTime.Now.Year;
+                var allLocations = new List<LeaveAllLocation>();
+
+                foreach (var employee in employees)
+                {
+                    if (await _leaveAllLocationRepository.AllLocationExists(employee.Id, leaveType.Id, period))
+                        continue;
+
+                    allLocations.Add(new LeaveAllLocation
+                    {
+                        EmployeeId = employee.Id,
+                        LeaveTypeId = leaveType.Id,
+                        NumbersOfDays = leaveType.DefaultDays,
+                        Period = period,
+                    });
+                }
+
+                await _leaveAllLocationRepository.AddLocation(allLocations);
             }
 
             var leaveAllLocation = _mapper.Map<LeaveAllLocation>(request.LeaveAllLocationDto);
