77a84ff6e77a4436b678cbc8f77d83a3be5cfad1
Nikita Ivakh <maffy145rus@mail.ru>
2023-12-09 09:58:17 +0300

    Create an Employee class. Create a service to get users

---
+++ HR.LeaveManagement.Identity/Services/UserService.cs
@@ -0,0 +1,29 @@
+﻿using HR.LeaveManagement.Application.Contracts.Identity;
+using HR.LeaveManagement.Application.Models.Identity;
+using HR.LeaveManagement.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace HR.LeaveManagement.Identity.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<List<Employee>> GetEmployeesAsync()
+        {
+            var emplyees = await _userManager.GetUsersInRoleAsync("Employee");
+            return emplyees.Select(key => new Employee
+            {
+                Id = key.Id,
+                Email = key.Email,
+                FirstName = key.FirstName,
+                LastName = key.LastName,
+            }).ToList();
+        }
+    }
+}
