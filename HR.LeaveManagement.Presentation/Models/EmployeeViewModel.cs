eee269efadc58d1f425345ca1915c8246b7c9e99
Nikita Ivakh <maffy145rus@mail.ru>
2023-12-10 11:06:49 +0300

    Create the EmployeeViewModel and LeaveRequestViewModel class

---
+++ HR.LeaveManagement.Presentation/Models/EmployeeViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace HR.LeaveManagement.Presentation.Models
+{
+    public class EmployeeViewModel
+    {
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ HR.LeaveManagement.Presentation/Models/LeaveRequestViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace HR.LeaveManagement.Presentation.Models
+{
+    public class LeaveRequestViewModel : CreateRequestViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Date Requested")]
+        public DateTime DateRequested { get; set; }
+
+        [Display(Name = "Date Actioned")]
+        public DateTime DateActioned { get; set; }
+
+        [Display(Name = "Approval State")]
+        public bool? Approved { get; set; }
+
+        public bool Cancelled { get; set; }
+
+        public LeaveTypeViewModel LeaveType { get; set; }
+
+        public EmployeeViewModel Employee { get; set; }
+    }
+
+    public class CreateRequestViewModel
+    {
+        [Required]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
+
+        public IEnumerable<SelectListItem> LeaveTypes { get; set; }
+
+        [Display(Name = "Leave Type")]
+        public int LeaveTypeId { get; set; }
+
+        [MaxLength(300)]
+        [Display(Name = "Comments")]
+        public string RequestComments { get; set; }
+    }
+}
