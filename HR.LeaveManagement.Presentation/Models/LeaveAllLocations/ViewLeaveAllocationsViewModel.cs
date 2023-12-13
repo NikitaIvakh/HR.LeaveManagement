ff08aa8331d4f906646241a90f6a69ab938f5207
Nikita Ivakh <maffy145rus@mail.ru>
2023-12-13 18:39:19 +0300

    Create a ViewLeaveAllocationsViewModel class

---
+++ HR.LeaveManagement.Presentation/Models/LeaveAllLocations/ViewLeaveAllocationsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HR.LeaveManagement.Presentation.Models.LeaveAllLocations
+{
+    public class ViewLeaveAllocationsViewModel
+    {
+        public string EmployeeId { get; set; }
+
+        public List<LeaveAllocationViewModel> LeaveAllocations { get; set; }
+    }
+}
