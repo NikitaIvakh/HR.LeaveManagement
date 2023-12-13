bbfc0afcb70836b77c2bb2a9eeba9ebb372954b2
Nikita Ivakh <maffy145rus@mail.ru>
2023-12-13 17:51:57 +0300

    Create the EmployeeLeaveRequestViewVM class

---
+++ HR.LeaveManagement.Presentation/Models/LeaveRequests/EmployeeLeaveRequestViewVM.cs
@@ -0,0 +1,7 @@
+﻿namespace HR.LeaveManagement.Presentation.Models.LeaveRequests
+{
+    public class EmployeeLeaveRequestViewVM
+    {
+        public List<LeaveRequestViewModel> LeaveRequests { get; set; }
+    }
+}
