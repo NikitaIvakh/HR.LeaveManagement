5fd2096ae3c793f5485de5584974adb6cd36d5fc
Nikita Ivakh <maffy145rus@mail.ru>
2023-12-13 17:11:26 +0300

    Update the details output implementation

--- HR.LeaveManagement.Application/Features/LeaveRequess/Handlers/Queries/GetLeaveRequestDetailsRequestHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveRequess/Handlers/Queries/GetLeaveRequestDetailsRequestHandler.cs
@@ -3,6 +3,7 @@ using HR.LeaveManagement.Application.DTOs.LeaveRequest;
 using HR.LeaveManagement.Application.Features.LeaveRequess.Requests.Queries;
 using HR.LeaveManagement.Application.Contracts.Persistence;
 using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Identity;
 
 namespace HR.LeaveManagement.Application.Features.LeaveRequess.Handlers.Queries
 {
@@ -10,17 +11,20 @@ namespace HR.LeaveManagement.Application.Features.LeaveRequess.Handlers.Queries
     {
         private readonly ILeaveRequestRepository _leaveRequestRepository;
         private readonly IMapper _mapper;
+        private readonly IUserService _userService;
 
-        public GetLeaveRequestDetailsRequestHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        public GetLeaveRequestDetailsRequestHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper, IUserService userService)
         {
             _leaveRequestRepository = leaveRequestRepository;
             _mapper = mapper;
+            _userService = userService;
         }
 
         public async Task<LeaveRequestDto> Handle(GetLeaveRequestDetailsRequest request, CancellationToken cancellationToken)
         {
-            var leaveRequest = await _leaveRequestRepository.GetLeaveRequestWithDetails(request.Id);
-            return _mapper.Map<LeaveRequestDto>(leaveRequest);
+            var leaveRequest = _mapper.Map<LeaveRequestDto>(await _leaveRequestRepository.GetLeaveRequestWithDetails(request.Id));
+            leaveRequest.Employee = await _userService.GetEmployeeAsync(leaveRequest.RequestingEmployeeId);
+            return leaveRequest;
         }
     }
 }
