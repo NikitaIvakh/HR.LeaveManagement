5e65c9936eeee07375446340f48748e957b1a6f2
Nikita Ivakh <maffy145rus@mail.ru>
2023-12-15 17:36:09 +0300

    Create a MockUnitOfWork class

---
+++ HR.LeaveManagement.Application.UnitTests/Mocks/MockUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.UnitTests.Common;
+using Moq;
+
+namespace HR.LeaveManagement.Application.UnitTests.Mocks
+{
+    public static class MockUnitOfWork
+    {
+        public static Mock<IUnitOfWork> GetUnitOfWorkMock()
+        {
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            var mockLeaveTypeRepository = MockLeaveTypeRepository.GetLeaveTypeRepository();
+
+            mockUnitOfWork.Setup(key => key.LeaveTypeRepository).Returns(mockLeaveTypeRepository.Object);
+            return mockUnitOfWork;
+        }
+    }
+}
