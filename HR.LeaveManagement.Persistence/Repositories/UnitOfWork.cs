bbf2d9190920a7a578883d413d433b1145d131ae
Nikita Ivakh <maffy145rus@mail.ru>
2023-12-15 16:26:51 +0300

    Implement the IUnitOfWork interface

---
+++ HR.LeaveManagement.Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,52 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+
+namespace HR.LeaveManagement.Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly HRLeaveManagementDbContext _context;
+        private ILeaveAllLocationRepository _leaveAllLocationRepository;
+        private ILeaveRequestRepository _leaveRequestRepository;
+        private ILeaveTypeRepository _leaveTypeRepository;
+
+        public UnitOfWork(HRLeaveManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public ILeaveAllLocationRepository LeaveAllLocationRepository
+        {
+            get
+            {
+                return _leaveAllLocationRepository ??= new LeaveAllLocationRepository(_context);
+            }
+        }
+
+        public ILeaveRequestRepository LeaveRequestRepository
+        {
+            get
+            {
+                return _leaveRequestRepository ??= new LeaveRequestRepository(_context);
+            }
+        }
+
+        public ILeaveTypeRepository LeaveTypeRepository
+        {
+            get
+            {
+                return _leaveTypeRepository ??= new LeaveTypeRepository(_context);
+            }
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
